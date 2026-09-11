4645bd06df56e33dc49a057ffe881cff3bc1d47b
Emmycode24 <chinomsoemmanuel98@gmail.com>
2026-09-11 12:34:13 +0100

    new project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -16,18 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { EventsProvider } from "./use-events";
-import "./index.css";
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <EventsProvider>
-    <App />
-  </EventsProvider>
-);
->>>>>>> 583d53d57011b56a51b5fb0e108698aa7ea9b18b
